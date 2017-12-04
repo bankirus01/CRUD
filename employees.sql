4996bb0cca0271f43e921b39463265aa128dc7c7
Andiy Petrik <bankirus01@gmail.com>
2017-12-04 21:08:44 +0200

    fixed error in config.php and poblem with database

--- employees.sql
+++ employees.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 16, 2017 at 10:53 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,15 +9,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `demo`
---
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `employees`
---
+
 
 CREATE TABLE IF NOT EXISTS `employees` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
